0ec79b2c5bc295c8a5de036cd6435eb333fcbb4e
3van0 <ouyafei1111@outlook.com>
2020-02-05 21:53:15 +0800

    添加项目文件。

---
+++ StartMenuTileEditor/formStartMenuTileEditor.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FCStartC
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FCStartC))
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.ColorSL = New System.Windows.Forms.Button()
+        Me.OpenFl = New System.Windows.Forms.Button()
+        Me.TRed = New System.Windows.Forms.TextBox()
+        Me.TGreen = New System.Windows.Forms.TextBox()
+        Me.TBlue = New System.Windows.Forms.TextBox()
+        Me.HX = New System.Windows.Forms.Label()
+        Me.BAppPth = New System.Windows.Forms.TextBox()
+        Me.FileSL = New System.Windows.Forms.Button()
+        Me.IconSL = New System.Windows.Forms.Button()
+        Me.BIconPTH = New System.Windows.Forms.TextBox()
+        Me.IconSLEN = New System.Windows.Forms.CheckBox()
+        Me.GO = New System.Windows.Forms.Button()
+        Me.pickclr = New System.Windows.Forms.PictureBox()
+        Me.pickcolor = New System.Windows.Forms.PictureBox()
+        CType(Me.pickclr, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pickcolor, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ColorSL
+        '
+        Me.ColorSL.Location = New System.Drawing.Point(152, 132)
+        Me.ColorSL.Name = "ColorSL"
+        Me.ColorSL.Size = New System.Drawing.Size(133, 48)
+        Me.ColorSL.TabIndex = 0
+        Me.ColorSL.Text = "取色器"
+        Me.ColorSL.UseVisualStyleBackColor = True
+        '
+        'OpenFl
+        '
+        Me.OpenFl.Location = New System.Drawing.Point(13, 132)
+        Me.OpenFl.Name = "OpenFl"
+        Me.OpenFl.Size = New System.Drawing.Size(133, 48)
+        Me.OpenFl.TabIndex = 2
+        Me.OpenFl.Text = "打开图片"
+        Me.OpenFl.UseVisualStyleBackColor = True
+        '
+        'TRed
+        '
+        Me.TRed.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.TRed.Location = New System.Drawing.Point(13, 214)
+        Me.TRed.Name = "TRed"
+        Me.TRed.Size = New System.Drawing.Size(86, 30)
+        Me.TRed.TabIndex = 3
+        '
+        'TGreen
+        '
+        Me.TGreen.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.TGreen.Location = New System.Drawing.Point(105, 214)
+        Me.TGreen.Name = "TGreen"
+        Me.TGreen.Size = New System.Drawing.Size(87, 30)
+        Me.TGreen.TabIndex = 4
+        '
+        'TBlue
+        '
+        Me.TBlue.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.TBlue.Location = New System.Drawing.Point(198, 214)
+        Me.TBlue.Name = "TBlue"
+        Me.TBlue.Size = New System.Drawing.Size(87, 30)
+        Me.TBlue.TabIndex = 5
+        '
+        'HX
+        '
+        Me.HX.AutoSize = True
+        Me.HX.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.HX.Location = New System.Drawing.Point(9, 258)
+        Me.HX.Name = "HX"
+        Me.HX.Size = New System.Drawing.Size(79, 20)
+        Me.HX.TabIndex = 9
+        Me.HX.Text = "#000000"
+        '
+        'BAppPth
+        '
+        Me.BAppPth.Font = New System.Drawing.Font("宋体", 10.0!)
+        Me.BAppPth.Location = New System.Drawing.Point(152, 12)
+        Me.BAppPth.Name = "BAppPth"
+        Me.BAppPth.Size = New System.Drawing.Size(489, 27)
+        Me.BAppPth.TabIndex = 10
+        '
+        'FileSL
+        '
+        Me.FileSL.Font = New System.Drawing.Font("宋体", 10.0!)
+        Me.FileSL.Location = New System.Drawing.Point(13, 10)
+        Me.FileSL.Name = "FileSL"
+        Me.FileSL.Size = New System.Drawing.Size(133, 31)
+        Me.FileSL.TabIndex = 11
+        Me.FileSL.Text = "选择应用程序"
+        Me.FileSL.UseVisualStyleBackColor = True
+        '
+        'IconSL
+        '
+        Me.IconSL.Enabled = False
+        Me.IconSL.Location = New System.Drawing.Point(13, 86)
+        Me.IconSL.Name = "IconSL"
+        Me.IconSL.Size = New System.Drawing.Size(133, 31)
+        Me.IconSL.TabIndex = 13
+        Me.IconSL.Text = "选择图标"
+        Me.IconSL.UseVisualStyleBackColor = True
+        '
+        'BIconPTH
+        '
+        Me.BIconPTH.Enabled = False
+        Me.BIconPTH.Font = New System.Drawing.Font("宋体", 10.0!)
+        Me.BIconPTH.Location = New System.Drawing.Point(152, 88)
+        Me.BIconPTH.Name = "BIconPTH"
+        Me.BIconPTH.Size = New System.Drawing.Size(489, 27)
+        Me.BIconPTH.TabIndex = 12
+        '
+        'IconSLEN
+        '
+        Me.IconSLEN.AutoSize = True
+        Me.IconSLEN.Font = New System.Drawing.Font("宋体", 10.0!)
+        Me.IconSLEN.Location = New System.Drawing.Point(13, 59)
+        Me.IconSLEN.Name = "IconSLEN"
+        Me.IconSLEN.Size = New System.Drawing.Size(98, 21)
+        Me.IconSLEN.TabIndex = 14
+        Me.IconSLEN.Text = "选择图标"
+        Me.IconSLEN.UseVisualStyleBackColor = True
+        '
+        'GO
+        '
+        Me.GO.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.GO.Location = New System.Drawing.Point(13, 346)
+        Me.GO.Name = "GO"
+        Me.GO.Size = New System.Drawing.Size(272, 136)
+        Me.GO.TabIndex = 0
+        Me.GO.Text = "运行"
+        Me.GO.UseVisualStyleBackColor = True
+        '
+        'pickclr
+        '
+        Me.pickclr.Image = Global.StartMenuTileEditor.My.Resources.Resources.th
+        Me.pickclr.InitialImage = CType(resources.GetObject("pickclr.InitialImage"), System.Drawing.Image)
+        Me.pickclr.Location = New System.Drawing.Point(291, 132)
+        Me.pickclr.Name = "pickclr"
+        Me.pickclr.Size = New System.Drawing.Size(350, 350)
+        Me.pickclr.TabIndex = 1
+        Me.pickclr.TabStop = False
+        '
+        'pickcolor
+        '
+        Me.pickcolor.Location = New System.Drawing.Point(13, 281)
+        Me.pickcolor.Name = "pickcolor"
+        Me.pickcolor.Size = New System.Drawing.Size(272, 50)
+        Me.pickcolor.TabIndex = 15
+        Me.pickcolor.TabStop = False
+        '
+        'FCStartC
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(653, 494)
+        Me.Controls.Add(Me.pickcolor)
+        Me.Controls.Add(Me.GO)
+        Me.Controls.Add(Me.IconSLEN)
+        Me.Controls.Add(Me.IconSL)
+        Me.Controls.Add(Me.BIconPTH)
+        Me.Controls.Add(Me.FileSL)
+        Me.Controls.Add(Me.BAppPth)
+        Me.Controls.Add(Me.HX)
+        Me.Controls.Add(Me.TBlue)
+        Me.Controls.Add(Me.TGreen)
+        Me.Controls.Add(Me.TRed)
+        Me.Controls.Add(Me.OpenFl)
+        Me.Controls.Add(Me.pickclr)
+        Me.Controls.Add(Me.ColorSL)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "FCStartC"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "StartMenuTileEditor"
+        CType(Me.pickclr, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pickcolor, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents ColorSL As Button
+    Friend WithEvents pickclr As PictureBox
+    Friend WithEvents OpenFl As Button
+    Friend WithEvents TRed As TextBox
+    Friend WithEvents TGreen As TextBox
+    Friend WithEvents TBlue As TextBox
+    Friend WithEvents HX As Label
+    Friend WithEvents BAppPth As TextBox
+    Friend WithEvents FileSL As Button
+    Friend WithEvents IconSL As Button
+    Friend WithEvents BIconPTH As TextBox
+    Friend WithEvents IconSLEN As CheckBox
+    Friend WithEvents GO As Button
+    Friend WithEvents pickcolor As PictureBox
+End Class
---
+++ StartMenuTileEditor/formStartMenuTileEditor.vb
@@ -0,0 +1,145 @@
+﻿Imports System.IO
+Public Class FCStartC
+    Dim FilePth, FileName, IconName, IconPth As String
+
+    Private Sub ColorSL_Click(sender As Object, e As EventArgs) Handles ColorSL.Click
+        Dim COL As Color
+        If ColorDialog1.ShowDialog() = DialogResult.OK Then
+            COL = ColorDialog1.Color
+            TRed.Text = COL.R.ToString
+            TGreen.Text = COL.G.ToString
+            TBlue.Text = COL.B.ToString
+            HX.Text = System.Drawing.ColorTranslator.ToHtml(COL)
+            pickcolor.BackColor = COL
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pickclr.MouseDown
+        Dim GetColor As Color = CType(pickclr.Image, Bitmap).GetPixel(e.X, e.Y) '使用GetPixel（）函数要先把图片转换成BitMap格式
+        TRed.Text = GetColor.R.ToString
+        TGreen.Text = GetColor.G.ToString
+        TBlue.Text = GetColor.B.ToString
+        HX.Text = System.Drawing.ColorTranslator.ToHtml(GetColor)
+        pickcolor.BackColor = GetColor
+    End Sub
+
+    Private Sub OpenFl_Click(sender As Object, e As EventArgs) Handles OpenFl.Click
+        OpenXFile()
+    End Sub
+
+    Private Sub OpenXFile()
+        Dim OpenDialog1 As New OpenFileDialog
+        OpenDialog1.Filter = "图片格式|*.jpg;*.jpeg;*.png;*.ico;*gif|所有文件*.*|*.*"
+        If OpenDialog1.ShowDialog = DialogResult.OK Then
+            Dim Path As String = OpenDialog1.FileName
+            Dim imgScr As Bitmap = New Bitmap(Path)
+            Dim imgDest As Bitmap = New Bitmap(pickclr.Width, pickclr.Height)
+            Dim g As Graphics = Graphics.FromImage(imgDest)
+            g.DrawImage(imgScr, New Rectangle(0, 0, pickclr.Width, pickclr.Height), New Rectangle(0, 0, imgScr.Width, imgScr.Height), GraphicsUnit.Pixel)
+            pickclr.Image = imgDest
+            g.Dispose()
+        End If
+    End Sub
+
+    Private Sub IconSLEN_CheckedChanged(sender As Object, e As EventArgs) Handles IconSLEN.CheckedChanged
+        If IconSLEN.Checked Then
+            IconSL.Enabled = True
+            BIconPTH.Enabled = True
+        ElseIf IconSLEN.Checked = False Then
+            IconSL.Enabled = False
+            BIconPTH.Enabled = False
+        End If
+    End Sub
+
+
+    Private Sub GO_Click(sender As Object, e As EventArgs) Handles GO.Click
+        If FilePth <> "" Then
+            'generate .xml file
+            Dim FileFinal As FileStream = New FileStream(FileName + ".VisualElementsManifest.xml", FileMode.Create, FileAccess.ReadWrite)
+            Dim names As String() = New String() {"<?xml version=" + Chr(34) + "1.0" + Chr(34) + " encoding=" + Chr(34) + "utf-8" + Chr(34) + "?>", "<Application xmlns:xsi=" + Chr(34) + "http://www.w3.org/2001/XMLSchema-instance" + Chr(34) + ">", "<VisualElements ", "ForegroundText=" + Chr(34) + "light" + Chr(34) + " "}
+            Dim s As String
+            Using sw As StreamWriter = New StreamWriter(FileFinal)
+                For Each s In names
+                    sw.WriteLine(s)
+                Next s
+                Dim swcolor As String = New String("BackgroundColor=" + Chr(34) + HX.Text + Chr(34))
+                sw.WriteLine(swcolor)
+                sw.WriteLine("ShowNameOnSquare150x150Logo=" + Chr(34) + "on" + Chr(34) + " ")
+                If IconSLEN.Checked Then
+                    If IconPth <> FilePth Then
+                        File.Copy(IconPth + "\" + IconName, FilePth + "\" + IconName, True)
+                    End If
+                    sw.WriteLine("Square150x150Logo=" + Chr(34) + IconName + Chr(34))
+                    sw.WriteLine("Square70x70Logo=" + Chr(34) + IconName + Chr(34))
+                End If
+                sw.WriteLine("></VisualElements></Application>")
+            End Using
+            'copy xml
+            File.Copy(FileName + ".VisualElementsManifest.xml", FilePth + "\" + FileName + ".VisualElementsManifest.xml", True)
+
+            'generate .bat file
+            Dim StartPath As String = New String("C:\ProgramData\Microsoft\Windows\Start Menu\Programs")
+            Dim username As String
+            username = Environment.GetEnvironmentVariable("USERNAME")
+            Dim Userstartpath As String = New String("C:\Users\" + username + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs")
+            Dim shellfile As FileStream = New FileStream("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\RS.bat", FileMode.Create, FileAccess.ReadWrite)
+            Using sws As StreamWriter = New StreamWriter(shellfile)
+                sws.WriteLine("chcp 65001")
+                Writedir(StartPath, sws)
+                Writedir(Userstartpath, sws)
+                sws.Close()
+            End Using
+
+            'run .bat file
+            Dim proc As New ProcessStartInfo()
+            proc.UseShellExecute = True
+            proc.WorkingDirectory = "C:\"
+            proc.FileName = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\RS.bat"
+            proc.Verb = "runas"
+            Process.Start(proc)
+        End If
+    End Sub
+
+    Private Sub BIconPTH_TextChanged(sender As Object, e As EventArgs) Handles BIconPTH.TextChanged
+
+    End Sub
+
+    Private Sub pickclr_Click(sender As Object, e As EventArgs) Handles pickclr.Click
+
+    End Sub
+
+    Private Sub IconSL_Click(sender As Object, e As EventArgs) Handles IconSL.Click
+        Dim opendialog1 As New OpenFileDialog
+        opendialog1.Filter = "PNG格式|*.png"
+        If opendialog1.ShowDialog = DialogResult.OK Then
+            Dim result As String
+            result = opendialog1.FileName
+            IconName = Path.GetFileName(result)
+            'IconName = FileName.Substring(0, FileName.LastIndexOf("."))
+            IconPth = Path.GetDirectoryName(result)
+            BIconPTH.Text = IconPth
+        End If
+    End Sub
+
+    Private Sub FileSL_Click(sender As Object, e As EventArgs) Handles FileSL.Click
+        Dim opendialog1 As New OpenFileDialog
+        opendialog1.Filter = "EXE格式|*.exe"
+        If opendialog1.ShowDialog = DialogResult.OK Then
+            Dim result As String
+            result = opendialog1.FileName
+            FileName = Path.GetFileName(result)
+            FileName = FileName.Substring(0, FileName.LastIndexOf("."))
+            FilePth = Path.GetDirectoryName(result)
+            BAppPth.Text = FilePth
+        End If
+    End Sub
+
+    Private Sub Writedir(path As String, sws As StreamWriter)
+        sws.WriteLine("cd " + path)
+        sws.WriteLine("for %%f in (*.*) do copy /b " + Chr(34) + "%%f" + Chr(34) + " +,,")
+        Dim dir As New DirectoryInfo(path)
+        For Each d As DirectoryInfo In dir.GetDirectories
+            Writedir(d.FullName, sws)
+        Next
+    End Sub
+End Class
